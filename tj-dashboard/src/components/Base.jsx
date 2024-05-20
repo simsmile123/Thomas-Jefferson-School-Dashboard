cb6e9b9ad26f01a20ddf63ca18b0e9560bf344e2
Jason Pham <deh5yb@virginia.edu>
2024-05-20 17:45:21 -0400

    Add sidebar navbar and footer

--- tj-dashboard/src/components/Base.jsx
+++ tj-dashboard/src/components/Base.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Box, Container, Typography } from '@mui/material';
 import { Outlet } from 'react-router-dom';
+import { Navbar } from './Navbar.jsx';
+import { Footer } from './Footer.jsx';
+import { SideBar } from './SideBar.jsx';
 export const Base = () => {
   return (
-    <Box>
-        <Outlet />
-    </Box>
+    <Box display="flex" flexDirection="column" minHeight="100vh">
+    <Navbar />
+    <Container component="main" sx={{ flex: 1 }}>
+      <Outlet />
+    </Container>
+    <Footer />
+  </Box>
   )
 }
 
--- tj-dashboard/src/components/Navbar.jsx
+++ tj-dashboard/src/components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {SideBar} from './SideBar.jsx';
 
 export const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [open, setOpen] = React.useState(false);
+
+    const toggleDrawer = (newOpen) => () => {
+      setOpen(newOpen);
+    };
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={toggleDrawer(true)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+          <SideBar open={open} setOpen={setOpen} toggleDrawer={toggleDrawer}></SideBar>
+        </Box>
+      );
 }
 
 
