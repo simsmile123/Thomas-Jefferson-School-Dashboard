719fe76b4d3b8ad8a447b12c8df7a39a4aaaf48b
Jason Pham <deh5yb@virginia.edu>
2024-05-23 12:02:14 -0400

    Fix class info merge conflicts

--- tj-dashboard/src/components/ClassInfo.jsx
+++ tj-dashboard/src/components/ClassInfo.jsx
@@ -15,23 +15,6 @@ import {
   DialogTitle,
   TextField,
 } from "@mui/material";
-import {
-  Box,
-  Button,
-  Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  TextField,
-} from "@mui/material";
 import React, { useState, useEffect } from "react";
 import * as dbc from "../../database-controller";
 
@@ -68,20 +51,6 @@ export const ClassInfo = ( {classID} ) => {
         });
       }
 
-      studentData.sort((a, b) => a.last_name.localeCompare(b.last_name));
-
-      const fetchAllStudentList = await dbc.getStudentsList();
-      const filteredStudentList = fetchAllStudentList.filter(
-        (student1) =>
-          !studentData.some((student2) => student2.id === student1.id)
-      );
-      setAllStudentList(filteredStudentList);
-
-      const fetchAllTeacherList = await dbc.getTeachersList();
-      const filteredTeacherList = fetchAllTeacherList.filter(
-        (teacher1) =>!teacherData.some((teacher2) => teacher2.id === teacher1.id)
-      );
-      setAllTeacherList(filteredTeacherList);
 
       studentData.sort((a, b) => a.last_name.localeCompare(b.last_name));
 
@@ -177,71 +146,6 @@ export const ClassInfo = ( {classID} ) => {
     }
   };
 
-
-  const handleGradeChange = async (studentID, newGrade) => {
-    try {
-      await dbc.updateStudentGrade(classID, studentID, newGrade);
-      setStudents(
-        students.map((student) =>
-          student.id === studentID ? { ...student, grade: newGrade } : student
-        )
-      );
-    } catch (error) {
-      console.error("Error updating student grade:", error);
-    }
-  };
-
-  const handleAddStudent = async (student) => {
-    try {
-      await dbc.addStudentToClass(classID, student.id);
-      setStudents((students) => [
-        ...students,
-        {
-          ...student,
-          grade: null,
-          first_name: student.first_name,
-          last_name: student.last_name,
-        },
-      ]);
-      setAllStudentList(allStudentList.filter((s) => s.id !== student.id));
-      setDialogAddStudent(false);
-    } catch (error) {
-      console.error("Error adding student:", error);
-    }
-  };
-
-  const handleRemoveStudent = async (student) => {
-    try {
-      await dbc.removeStudentFromClass(classID, student.id);
-      setStudents(students.filter((s) => s.id !== student.id));
-      setAllStudentList([...allStudentList, student]);
-      setDialogRemoveStudent(false);
-    } catch (error) {
-      console.error("Error removing student:", error);
-    }
-  };
-
-  const handleAddTeacher = async (teacher) => {
-    try {
-      await dbc.addTeacherToClass(classID, teacher.id);
-      setTeachers([...teachers, teacher]);
-      setAllTeacherList(allTeacherList.filter((t) => t.id !== teacher.id));
-      setDialogAddTeacher(false);
-    } catch (error) {
-      console.error("Error adding teacher:", error);
-    }
-  };
-
-  const handleRemoveTeacher = async (teacher) => {
-    try {
-      await dbc.removeTeacherFromClass(classID, teacher.id);
-      setTeachers(teachers.filter((t) => t.id !== teacher.id));
-      setAllTeacherList([...allTeacherList, teacher]);
-      setDialogRemoveTeacher(false);
-    } catch (error) {
-      console.error("Error removing teacher:", error);
-    }
-  };
 
   return (
     <Box>
@@ -476,64 +380,9 @@ const calculateAverageGrade = (students) => {
     gradedStudents.reduce((acc, student) => acc + student.grade, 0) /
     gradedStudents.length
   ).toFixed(2);
-  );
 };
 
 
-const calculateAverageGrade = (students) => {
-  const gradedStudents = students.filter((student) => student.grade !== null);
-  if (gradedStudents.length === 0) return "N/A";
-  return (
-    gradedStudents.reduce((acc, student) => acc + student.grade, 0) /
-    gradedStudents.length
-  ).toFixed(2);
-};
-
-const DialogBox = ({ peopleList, dialogOpen, setDialogOpen, handleAction, subject, action }) => {
-  return (
-    <Dialog
-      open={dialogOpen}
-      onClose={() => setDialogOpen(false)}
-    >
-      <DialogTitle>{`${action} ${subject}`}</DialogTitle>
-      <DialogContent>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>{subject} Name</TableCell>
-                <TableCell>{action}</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {peopleList.map((person) => (
-                <TableRow key={person.id}>
-                  <TableCell>
-                    {person.last_name}, {person.first_name}
-                  </TableCell>
-                  <TableCell>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => handleAction(person)}
-                    >
-                      {action}
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={() => setDialogOpen(false)} color="primary">
-          Cancel
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
 const DialogBox = ({ peopleList, dialogOpen, setDialogOpen, handleAction, subject, action }) => {
   return (
     <Dialog
