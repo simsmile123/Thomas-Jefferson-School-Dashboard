d2c878e521411d4b511aa8ffb7405ce5fece3cd3
Jason Pham <deh5yb@virginia.edu>
2024-05-23 11:53:02 -0400

    add teacher add/remove to class

--- tj-dashboard/src/components/ClassInfo.jsx
+++ tj-dashboard/src/components/ClassInfo.jsx
@@ -24,9 +24,12 @@ export const ClassInfo = ( {classID} ) => {
   const [teachers, setTeachers] = useState([]);
   const [students, setStudents] = useState([]);
   const [allStudentList, setAllStudentList] = useState([]);
-  const [dialogOpenAdd, setDialogOpenAdd] = useState(false);
-  const [dialogOpenRemove, setDialogOpenRemove] = useState(false);
-  const classID = "I99sXFrMAw5qPXl3UuVq";
+  const [allTeacherList, setAllTeacherList] = useState([]);
+  const [dialogAddStudent, setDialogAddStudent] = useState(false);
+  const [dialogRemoveStudent, setDialogRemoveStudent] = useState(false);
+  const [dialogAddTeacher, setDialogAddTeacher] = useState(false);
+  const [dialogRemoveTeacher, setDialogRemoveTeacher] = useState(false);
+
 
   const fetchClassInfo = async () => {
     try {
@@ -48,13 +51,20 @@ export const ClassInfo = ( {classID} ) => {
         });
       }
 
-      studentData.sort((a,b) => a.last_name.localeCompare(b.last_name))
+      studentData.sort((a, b) => a.last_name.localeCompare(b.last_name));
 
       const fetchAllStudentList = await dbc.getStudentsList();
       const filteredStudentList = fetchAllStudentList.filter(
-        (student1) => !studentData.some((student2) => student2.id === student1.id)
+        (student1) =>
+          !studentData.some((student2) => student2.id === student1.id)
       );
       setAllStudentList(filteredStudentList);
+
+      const fetchAllTeacherList = await dbc.getTeachersList();
+      const filteredTeacherList = fetchAllTeacherList.filter(
+        (teacher1) =>!teacherData.some((teacher2) => teacher2.id === teacher1.id)
+      );
+      setAllTeacherList(filteredTeacherList);
 
       setClassInfo(classInfoData);
       setTeachers(teacherData);
@@ -68,43 +78,34 @@ export const ClassInfo = ( {classID} ) => {
     fetchClassInfo();
   }, []);
 
-  const handleDialogueOpenAdd = () => {
-    setDialogOpenAdd(true);
-  };
-
-  const handleDialogCloseAdd = () => {
-    setDialogOpenAdd(false);
-  };
-
-  const handleDialogueOpenRemove = () => {
-    setDialogOpenRemove(true);
-  };
-
-  const handleDialogCloseRemove = () => {
-    setDialogOpenRemove(false);
-  };
 
   const handleGradeChange = async (studentID, newGrade) => {
     try {
       await dbc.updateStudentGrade(classID, studentID, newGrade);
       setStudents(
         students.map((student) =>
-          student.id === studentID ? { ...student, grade: newGrade  } : student
+          student.id === studentID ? { ...student, grade: newGrade } : student
         )
       );
     } catch (error) {
       console.error("Error updating student grade:", error);
     }
   };
-  
-
 
   const handleAddStudent = async (student) => {
     try {
       await dbc.addStudentToClass(classID, student.id);
-      setStudents((students) => [...students, { ...student, grade: null, first_name: student.firstname, last_name: student.lastname}]);
-      setDialogOpenAdd(false);
-
+      setStudents((students) => [
+        ...students,
+        {
+          ...student,
+          grade: null,
+          first_name: student.first_name,
+          last_name: student.last_name,
+        },
+      ]);
+      setAllStudentList(allStudentList.filter((s) => s.id !== student.id));
+      setDialogAddStudent(false);
     } catch (error) {
       console.error("Error adding student:", error);
     }
@@ -113,160 +114,210 @@ export const ClassInfo = ( {classID} ) => {
   const handleRemoveStudent = async (student) => {
     try {
       await dbc.removeStudentFromClass(classID, student.id);
-      setDialogOpenAdd(false);
-
+      setStudents(students.filter((s) => s.id !== student.id));
+      setAllStudentList([...allStudentList, student]);
+      setDialogRemoveStudent(false);
     } catch (error) {
-      console.error("Error adding student:", error);
+      console.error("Error removing student:", error);
     }
   };
 
-  useEffect(() => {
-    setAllStudentList(allStudentList.filter(
-      (student1) => !students.some((student2) => student2.id === student1.id)
-    ));
-    console.log(students)
-  }, [students])
+  const handleAddTeacher = async (teacher) => {
+    try {
+      await dbc.addTeacherToClass(classID, teacher.id);
+      setTeachers([...teachers, teacher]);
+      setAllTeacherList(allTeacherList.filter((t) => t.id !== teacher.id));
+      setDialogAddTeacher(false);
+    } catch (error) {
+      console.error("Error adding teacher:", error);
+    }
+  };
 
+  const handleRemoveTeacher = async (teacher) => {
+    try {
+      await dbc.removeTeacherFromClass(classID, teacher.id);
+      setTeachers(teachers.filter((t) => t.id !== teacher.id));
+      setAllTeacherList([...allTeacherList, teacher]);
+      setDialogRemoveTeacher(false);
+    } catch (error) {
+      console.error("Error removing teacher:", error);
+    }
+  };
 
-
-  
   return (
-      <Box>
-        <Typography variant="h2">{classInfo.name}</Typography>
-        <Typography variant="h4">Grade: {classInfo.grade}</Typography>
+    <Box>
+      <Typography variant="h2">{classInfo.name}</Typography>
+      <Typography variant="h4">Grade: {classInfo.grade}</Typography>
 
-        <Typography variant="h4">Teachers:</Typography>
-        {teachers.map((teacher, index) => (
-          <Typography key={index} variant="h6">
-            {teacher.last_name}, {teacher.first_name}
-          </Typography>
-        ))}
-
-        <Typography variant="h4">
-          Average Grade:{" "}
-          {calculateAverageGrade(students)}
+      <Typography variant="h4">Teachers:</Typography>
+      {teachers.map((teacher, index) => (
+        <Typography key={index} variant="h6">
+          {teacher.last_name}, {teacher.first_name}
         </Typography>
+      ))}
 
-        <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
-          <Button variant="contained" color="primary" onClick={handleDialogueOpenAdd}>
+      <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setDialogAddTeacher(true)}
+        >
+          Add Teacher
+        </Button>
+
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setDialogRemoveTeacher(true)}
+        >
+          Remove Teacher
+        </Button>
+      </Box>
+
+      <Typography variant="h4">
+        Average Grade: {calculateAverageGrade(students)}
+      </Typography>
+
+      <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setDialogAddStudent(true)}
+        >
           Add Student
-          </Button>
+        </Button>
 
-          <Button variant="contained" color="primary" onClick={handleDialogueOpenRemove}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setDialogRemoveStudent(true)}
+        >
           Remove Student
-          </Button>
-        </Box>
+        </Button>
+      </Box>
 
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Student Name</TableCell>
-                <TableCell align="right">Grade</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {students.map((student) => (
-                <TableRow key={student.id}>
-                  <TableCell>{`${student.last_name}, ${student.first_name} `}</TableCell>
-                  <TableCell align="right">
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Student Name</TableCell>
+              <TableCell align="right">Grade</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {students.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell>{`${student.last_name}, ${student.first_name} `}</TableCell>
+                <TableCell align="right">
                   <TextField
                     type="number"
                     value={student.grade !== null ? student.grade : ""}
                     placeholder={student.grade === null ? "N/A" : ""}
-                    onChange={(e) => handleGradeChange(student.id, parseFloat(e.target.value) || null)}
+                    onChange={(e) =>
+                      handleGradeChange(
+                        student.id,
+                        parseFloat(e.target.value) || null
+                      )
+                    }
                   />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <DialogBox
+        peopleList={allStudentList}
+        dialogOpen={dialogAddStudent}
+        setDialogOpen={setDialogAddStudent}
+        handleAction={handleAddStudent}
+        subject="Student"
+        action="Add"
+      />
+
+      <DialogBox
+        peopleList={students}
+        dialogOpen={dialogRemoveStudent}
+        setDialogOpen={setDialogRemoveStudent}
+        handleAction={handleRemoveStudent}
+        subject="Student"
+        action="Remove"
+      />
+
+      <DialogBox
+        peopleList={allTeacherList}
+        dialogOpen={dialogAddTeacher}
+        setDialogOpen={setDialogAddTeacher}
+        handleAction={handleAddTeacher}
+        subject="Teacher"
+        action="Add"
+      />
+
+      <DialogBox
+        peopleList={teachers}
+        dialogOpen={dialogRemoveTeacher}
+        setDialogOpen={setDialogRemoveTeacher}
+        handleAction={handleRemoveTeacher}
+        subject="Teacher"
+        action="Remove"
+      />
+    </Box>
+  );
+};
+
+
+const calculateAverageGrade = (students) => {
+  const gradedStudents = students.filter((student) => student.grade !== null);
+  if (gradedStudents.length === 0) return "N/A";
+  return (
+    gradedStudents.reduce((acc, student) => acc + student.grade, 0) /
+    gradedStudents.length
+  ).toFixed(2);
+};
+
+const DialogBox = ({ peopleList, dialogOpen, setDialogOpen, handleAction, subject, action }) => {
+  return (
+    <Dialog
+      open={dialogOpen}
+      onClose={() => setDialogOpen(false)}
+    >
+      <DialogTitle>{`${action} ${subject}`}</DialogTitle>
+      <DialogContent>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>{subject} Name</TableCell>
+                <TableCell>{action}</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {peopleList.map((person) => (
+                <TableRow key={person.id}>
+                  <TableCell>
+                    {person.last_name}, {person.first_name}
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => handleAction(person)}
+                    >
+                      {action}
+                    </Button>
                   </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-
-        <Dialog open={dialogOpenAdd} onClose={handleDialogCloseAdd}>
-        <DialogTitle>Add Student to Class</DialogTitle>
-        <DialogContent>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Student Name</TableCell>
-                  <TableCell>Add</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {allStudentList.map((student) => (
-                  <TableRow key={student.id}>
-                    <TableCell>
-                      {student.lastname}, {student.firstname}
-                    </TableCell>
-                    <TableCell>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => handleAddStudent(student)}
-                      >
-                        Add
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogCloseAdd} color="primary">
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-      <Dialog open={dialogOpenRemove} onClose={handleDialogCloseRemove}>
-        <DialogTitle>Remove Student to Class</DialogTitle>
-        <DialogContent>
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Student Name</TableCell>
-                  <TableCell>Remove</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {students.map((student) => (
-                  <TableRow key={student.id}>
-                    <TableCell>
-                      {student.last_name}, {student.first_name}
-                    </TableCell>
-                    <TableCell>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => handleRemoveStudent(student)}
-                      >
-                        Remove
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogCloseRemove} color="primary">
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-
-      </Box>
-    );
-};
-
-const calculateAverageGrade = (students) => {
-  const gradedStudents = students.filter(student => student.grade !== null);
-  if (gradedStudents.length === 0) return "N/A";
-  return (gradedStudents.reduce((acc, student) => acc + student.grade, 0) / gradedStudents.length).toFixed(2);
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setDialogOpen(false)} color="primary">
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 };
